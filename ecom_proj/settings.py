325c7328c2b6ec8c4cc52c556da094bdd0b1c24c
junior's signature <chibuzor.john.2018@gmail.com>
2025-01-20 14:13:30 -0800

    updated settings

--- ecom_proj/settings.py
+++ ecom_proj/settings.py
@@ -168,10 +168,10 @@ USE_TZ = True
 #     ]
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_ROOT = os.path.join(BASE_DIR, “staticfiles_build”, “static”)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
-STATIC_URL = “/staticfiles/”
-STATICFILES_DIRS = [os.path.join(BASE_DIR, “static”)]
+STATIC_URL = "/staticfiles/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
