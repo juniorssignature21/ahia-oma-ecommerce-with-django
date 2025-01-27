38ea7e4155678264cd082492bc8c88c453bd8406
Junior's Signature <162041570+juniorssignature21@users.noreply.github.com>
2025-01-27 17:25:33 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ echo "Building project packages."
 python3 -m pip install -r requirements.txt
 
 echo "Migrating Database..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python3 manage.py makemigrations --no-input
+python3 manage.py migrate --no-input
 
 echo "Collecting static files..."
 
-python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --no-input
