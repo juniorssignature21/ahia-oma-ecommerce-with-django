5ea38f5d46d99aa887190c720299d892b3ee5a0a
Junior's Signature <162041570+juniorssignature21@users.noreply.github.com>
2025-02-01 08:44:02 +0100

    Update settings.py

--- ecom_proj/settings.py
+++ ecom_proj/settings.py
@@ -112,20 +112,20 @@ WSGI_APPLICATION = 'ecom_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+#DATABASES = {
+#    'default': dj_database_url.parse(
+#        os.environ.get("DATABASE_URL", #f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+ #       conn_max_age=600,
+        conn_health_checks=True,
+#    )
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
